0e849cd823657d7c1624cd68a5b06cc2965654d0
Hector Abreu <alu0101068855@ull.edu.es>
2022-05-16 09:45:04 +0000

    Realizando modificacion

---
+++ src/index.ts
@@ -0,0 +1,89 @@
+import {Document, connect, model, Schema} from 'mongoose';
+
+connect('mongodb://127.0.0.1:27017/dsi-assessment', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+}).then(() => {
+  console.log('Connected to the database');
+}).catch(() => {
+  console.log('Something went wrong when conecting to the database');
+});
+
+interface UserInterface extends Document {
+  nombre: string,
+  apellidos: string,
+  edad: number,
+  email: string,
+  contraseña: string
+}
+const UserSchema = new Schema<UserInterface>({
+  nombre: {
+    type: String,
+    required: true,
+  },
+  apellidos: {
+    type: String,
+    required: true,
+  },
+  edad: {
+    type: Number,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  contraseña: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = model<UserInterface>('User', UserSchema);
+
+const user = new User({
+  nombre: 'Juan',
+  apellidos: 'Sánchez Gutiérrez',
+  edad: 24,
+  email: 'alu0101068853@ull.edu.es',
+  contraseña: '1234'
+});
+
+/**
+ * Añadir usuario
+ */
+user.save().then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
+
+/**
+ * Buscar usuario
+User.findOne({email: "alu0101068855@ull.edu.es"}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
+ */
+
+/**
+ * Actualizar usuario 
+User.updateOne({email: "alu0101068855@ull.edu.es", nombre: "Paco", apellidos: "Acosta Abreu"}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
+ */
+
+/**
+ * Eliminar usuario
+User.deleteOne({email: "alu0101068855@ull.edu.es"}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
+ */
+
--- src/modi.ts
+++ src/modi.ts
@@ -1,27 +1,77 @@
 import {MongoClient} from 'mongodb';
 
 const dbURL = 'mongodb://127.0.0.1:27017';
-const dbName = 'notes-app';
+const dbName = 'dsi-assessment';
 
-interface NoteInterface {
-  title: string,
-  body: string,
-  color: 'blue' | 'green' | 'red' | 'yellow' | 'magenta'
+interface UserInterface {
+  nombre: string,
+  apellidos: string,
+  edad: number,
+  email: string,
+  contraseña: string
 }
-
+/**
+ * Añadir un usuario
+ */
 MongoClient.connect(dbURL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then((client) => {
   const db = client.db(dbName);
 
-  return db.collection<NoteInterface>('notes').insertOne({
-    title: 'Red note',
-    body: 'This is a red note',
-    color: 'red',
+  return db.collection<UserInterface>('users').insertOne({
+    nombre: 'Hector',
+    apellidos: 'Abreu Acosta',
+    edad: 22,
+    email: 'alu0101068855@ull.edu.es',
+    contraseña: '1234'
   });
 }).then((result) => {
   console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
+
+/**
+ * Buscar usuario
+ */
+ MongoClient.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db(dbName);
+
+  return db.collection<UserInterface>('users').find({
+    email: 'alu0101068855@ull.edu.es',
+  }).toArray();
+}).then((result) => {
+  console.log(result);
+}).catch((error) => {
+  console.log(error);
+});
+
+/**
+ * Actualizar un usuario mediante e-mail
+ */
+ MongoClient.connect(dbURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then((client) => {
+  const db = client.db(dbName);
+
+  return db.collection<UserInterface>('users').updateOne({
+    email_: 'alu0101068855@ull.edu.es',
+  }, {
+    $set: {
+        nombre: 'Hector',
+        apellidos: 'Abreu Acosta',
+        edad: 22,
+        email: 'alu0101068855@ull.edu.es', 
+        contraseña: '4321'
+    },
+  });
+}).then((result) => {
+  console.log(result.modifiedCount);
 }).catch((error) => {
   console.log(error);
 });
