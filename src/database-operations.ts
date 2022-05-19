c6635c829f4c31ee7856ebfe58b7ecac0c1c29d3
Marcos Padilla <alu0101045177@ull.edu.es>
2022-05-19 10:37:16 +0000

    modif

---
+++ src/database-operations.ts
@@ -0,0 +1,33 @@
+//import * as moongose from 'moongose'
+
+/*
+import {Document, connect, model, Schema, Model} from 'mongoose';
+import { UserInterface } from './index';
+
+
+export class DBOpertaions {
+  USER: Model<UserInterface>
+  constructor(importedUser: Model<UserInterface>) {
+    this.USER = importedUser;
+
+    const user = new User({
+      nombre: 'Marcos',
+      apellidos: 'Padilla',
+      edad: 22,
+      email: 'alu0101045177@ull.edu.es',
+      asignaturas: 'dsi',
+      NIF: '0000000A'
+    });
+
+  }
+
+  AddUser() {
+    user.save().then((result) => {
+      console.log(result);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+}
+
+*/
